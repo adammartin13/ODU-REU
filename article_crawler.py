2e85e388ec8c88657d6c9eeede52e16ee115b173
Christopher Martin <christopheradammartin13@gmail.com>
2023-07-03 11:45:38 -0400

    Switched to urllib

--- article_crawler.py
+++ article_crawler.py
@@ -1,12 +1,26 @@
 import csv
+import urllib.error
+
 import requests
+from urllib.request import urlopen
 import json
 
 with open('Disinformation Training Data.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
 
     for row in reader:
-        print(row[0])
-        resp = requests.get(row[0])
-        print(resp.status_code)
-        print(resp.text)
+        # resp = requests.get(row[0])
+        data = {
+            "Link": row[0],
+            "Return Code": "",
+            "Publisher": "",
+            "Is Disinformation": row[1],
+            "Text": ""
+        }
+        # print(data)
+        try:
+            resp = urlopen(row[0])
+            print(resp.getcode())
+        except urllib.error.HTTPError as e:
+            print(e)
+
