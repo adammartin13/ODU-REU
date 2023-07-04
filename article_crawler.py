334c18f533edc7ce5d9f485037735016d9b52968
Christopher Martin <christopheradammartin13@gmail.com>
2023-07-04 19:01:10 -0400

    Added text extraction updates

--- article_crawler.py
+++ article_crawler.py
@@ -7,6 +7,9 @@ import json
 with open('Disinformation Training Data.csv', 'r') as csv_file:
     reader = csv.reader(csv_file)
 
+    viol_end_char = ['}', ')', ';', '=', '>', '_']
+    viol_start_char = ["'", ';', '/', '%', '+', '(', '!', '{', '&', '`', '=']
+
     for row in reader:
         data = {
             "Link": row[0],
@@ -19,8 +22,36 @@ with open('Disinformation Training Data.csv', 'r') as csv_file:
             resp = urlopen(row[0])
             data["Return Code"] = resp.getcode()
             print(data)
-            # print(resp.read())
+            html = resp.read().decode("utf8").split('<')
+            # text = ''
+            for line in html:
+                line = line[line.find('>')+1:]
+                while '\n' in line:
+                    line = line.replace('\n', '')  # Remove newline
+                while '\t' in line:
+                    line = line.replace('\t', '')  # Remove tabs
+                while '  ' in line:
+                    line = line.replace('  ', '')  # Remove space-tabs
+                try:
+                    while line[0] == ' ':
+                        line = line[1:]  # Remove leading spaces
+
+                    # line[-2] also checks for lines with extending newline operators
+                    # line.isnumeric() checks if line is a numeric value
+                    if line[-2] not in viol_end_char and line[-1] not in viol_end_char and not line.isnumeric():
+                        if line[0] not in viol_start_char:
+                            if line[0:4] != 'http' and line[0:3] != 'var' and \
+                                    line[0:4] != 'icon' and line[0:6] != 'window' and \
+                                    line[0:8] != 'document' and line[0:6] != 'jQuery':
+                                # text += line
+                                print(line)
+                except IndexError as e:
+                    error = e
+            # print(text)
+            resp.close()
         except urllib.error.HTTPError as e:
             data["Return Code"] = 403
             print(data)
-
+        except UnicodeDecodeError as e:
+            data["Return Code"] = 403
+            print(data)
